bc69ffc86da47a72e6a39514994b6060196509d2
Alex Grover <alexgrover@microsoft.com>
2023-11-06 14:02:01 +0000

    soultion completed

---
+++ GetRepoUpdates.cs
@@ -0,0 +1,62 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace groveale
+{
+    public static class GetRepoUpdates
+    {
+        [FunctionName("GetRepoUpdates")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string repoOwner = req.Query["repoOwner"];
+            string repoName = req.Query["repoName"];
+            string folderPath = req.Query["folderPath"];
+            string debugCommit = req.Query["debugCommit"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            repoOwner = repoOwner ?? data?.repoOwner;
+            repoName = repoName ?? data?.repoName;
+            folderPath = folderPath ?? data?.folderPath;
+            debugCommit = debugCommit ?? data?.debugCommit;
+
+            if (string.IsNullOrEmpty(repoOwner) || string.IsNullOrEmpty(repoName))
+            {
+                return new BadRequestObjectResult("Please pass a repoOwner, repoName on the query string or in the request body");
+            }
+
+            var watcher = new GitHubWatcher(repoOwner, repoName, folderPath);
+
+            try {
+
+                if (!string.IsNullOrEmpty(debugCommit))
+                {
+                    var commit = await watcher.GetCommit(debugCommit);
+                    return new OkObjectResult(commit);
+                }
+
+                var commits = await watcher.GetCommitsInLast24HoursAsync();
+
+                var commitDetails = await watcher.GetCommitDetails(commits);
+
+                var responseMessage = $"There were {commits.Count} commits in the last 24 hours.";
+
+                return new OkObjectResult(commitDetails);
+            }
+            catch (Exception ex) {
+                return new BadRequestObjectResult(ex.Message);
+            }
+        }
+    }
+}
---
+++ Helpers/GitHubWatcher.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+public class GitHubWatcher
+{
+    private readonly HttpClient httpClient;
+    private readonly string _repoOwner;
+    private readonly string _repoName;
+    private readonly string _folderPath;
+    private readonly string _debug;
+
+    public GitHubWatcher(string repoOwner, string repoName, string folderPath)
+    {
+        _repoOwner = repoOwner;
+        _repoName = repoName;
+        _folderPath = folderPath;
+
+        httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("groveale", "1.0"));
+        // string token = Environment.GetEnvironmentVariable("personalAccessToken");
+        // string b64Pat = Convert.ToBase64String(Encoding.UTF8.GetBytes($":{token}"));
+        // httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", b64Pat);
+    }
+
+
+    public async Task<CommitFilesItem> GetCommit(string id)
+    {
+        var apiUrl = $"https://api.github.com/repos/{_repoOwner}/{_repoName}/commits/{id}";
+        var response = await httpClient.GetAsync(apiUrl);
+        response.EnsureSuccessStatusCode();
+        var commit = await response.Content.ReadAsAsync<CommitFilesItem>();
+        
+        return commit;
+    }
+
+    public async Task<List<string>> GetCommitsInLast24HoursAsync()
+    {
+
+        var apiUrl = $"https://api.github.com/repos/{_repoOwner}/{_repoName}/commits";
+        if (!string.IsNullOrEmpty(_folderPath))
+        {
+            apiUrl += $"?path={_folderPath}";
+        }
+        var response = await httpClient.GetAsync(apiUrl);
+        response.EnsureSuccessStatusCode();
+        var commits = await response.Content.ReadAsAsync<List<CommitItem>>();
+        var commitsInLast24Hours = new List<string>();
+        foreach (var commit in commits)
+        {
+            if (DateTimeOffset.UtcNow - commit.Commit.Author.Date <= TimeSpan.FromHours(24))
+            {
+                commitsInLast24Hours.Add(commit.Sha);
+            }
+        }
+        return commitsInLast24Hours;
+    }
+
+    public async Task<List<CommitFilesItem>> GetCommitDetails(List<string> commitIds)
+    {
+        var commitDetails = new List<CommitFilesItem>();
+
+        foreach (var commitId in commitIds)
+        {
+            var commit = await GetCommit(commitId);
+            commitDetails.Add(commit);
+        }
+
+        return commitDetails;
+    }
+
+    public class CommitItem
+    {
+        public string Sha { get; set; }
+        public CommitDetails Commit { get; set; }
+    }
+
+    public class CommitDetails
+    {
+        public CommitAuthor Author { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class CommitAuthor
+    {
+        public DateTimeOffset Date { get; set; }
+    }
+
+    public class CommitFilesItem
+    {
+        public string Sha { get; set; }
+        public CommitDetails Commit { get; set; }
+
+        public List<CommitFile> Files { get; set; }
+    }
+
+    public class CommitFile
+    {
+        public string FileName { get; set; }
+        public string Status { get; set; }
+    }
+}
